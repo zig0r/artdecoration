16c4599bfe227ff9a702a6f7a3d3fcceeb1ab441
Olena-Stotska <i.olena.stotska@gmail.com>
2020-02-11 16:22:31 +0200

    Setup: moved our began project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="logo" src="https://logowiki.net/wp-content/uploads/imgp/Graphics-and-Prints-Logo-1-6714.png" />
+      <div><h1>Safe Life</h1></div>
+    </header>
+  )
+}
+
+export default Header;
