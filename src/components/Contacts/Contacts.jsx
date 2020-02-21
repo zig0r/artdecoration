a83f249dba3fa5b97d88af238d8a7a5839b35b81
Olena-Stotska <i.olena.stotska@gmail.com>
2020-02-21 15:50:33 +0200

    Feat(contact): adds text to page

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
-import { FormattedMessage as T } from 'react-intl';
+import s from './Contacts.module.scss'
+import { 
+  FormattedMessage as T, 
+  FormattedHTMLMessage as Thtml, 
+  useIntl 
+} from 'react-intl';
 
 const Contacts = () => {
+  const intl = useIntl();
+
   return (
-    <div>Contacts</div>
+    <div className="content">
+      <h3 className="container-style">
+        <T id="contact.name" />
+      </h3>
+
+      <article className={s.container}>
+        <div>
+          <h3>Пишіть:</h3>
+          <form>
+            <div class="control-group">
+              <label><T id="contact.form.name" />:</label>
+              <div class="controls">
+                <input type="text" name="feedback[name]" />                                    
+              </div>
+            </div>
+
+            <div class="control-group">
+              <label><T id="contact.form.email" />:</label>
+              <div class="controls">
+                <input type="text" name="feedback[email]" />                                    
+              </div>
+            </div>
+
+            <div class="control-group">
+              <label><T id="contact.form.text" />:</label>
+              <div class="controls">
+                <textarea rows="6" cols="30" name="feedback[message]" />                                    
+              </div>
+            </div>
+                    
+            <button type="submit" class="btn"><T id="contact.form.send" /></button>
+          </form>
+        </div>
+            
+        <div>
+          <h3><T id="contact.phone" />:</h3>
+          <div className={s.phones}>
+            {intl.messages.raw.menu.phones
+              .map((phone, index) => <div key={index} dangerouslySetInnerHTML={{ __html: phone }} />)}
+          </div>
+          <div className="markdown">
+            <Thtml id="contact.details" />
+          </div>
+        </div>
+      </article>
+    </div>
   )
 }
 
