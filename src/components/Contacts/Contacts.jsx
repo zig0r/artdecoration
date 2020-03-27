b2208714d2ec6ded279e875995a407df156dc7c7
Olena-Stotska <i.olena.stotska@gmail.com>
2020-03-27 15:36:16 +0200

    Feat(contacts); finish page

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import s from './Contacts.module.scss'
+import { Form } from 'react-html5-form';
+import FormInput from '../FormInput/FormInput';
+import s from './Contacts.module.scss';
 import {
   FormattedMessage as T,
   FormattedHTMLMessage as Thtml,
   useIntl
 } from 'react-intl';
+import { useState } from 'react';
 
-const Contacts = () => {
+const ContactInput = FormInput.scoped({
+  scope: 'contact.form',
+  className: s.controlGroup,
+});
+
+const DEFAULT_VALUES = {
+  name: '',
+  email: '',
+  message: '',
+};
+
+export default () => {
   const intl = useIntl();
+  const [values, setValues] = useState(DEFAULT_VALUES);
+  const setValue = prop => event => setValues({ ...values, [prop]: event.target.value });
+
+  function submit() {
+    // TODO: send request to server
+    console.log(values)
+    setValues(DEFAULT_VALUES)
+    return new Promise((resolve) => {
+      setTimeout(resolve, 2000);
+    });
+  }
 
   return (
     <div className="content">
@@ -16,41 +41,46 @@ const Contacts = () => {
       </h3>
 
       <article className={s.container}>
-        <div>
-          <h3>Пишіть:</h3>
-          <form>
-            <div className="control-group">
-              <label><T id="contact.form.name" />:</label>
-              <div className="controls">
-                <input type="text" name="feedback[name]" />
-              </div>
-            </div>
+        <div className={s.block}>
+          <h3><T id="contact.form.title" />:</h3>
 
-            <div className="control-group">
-              <label><T id="contact.form.email" />:</label>
-              <div className="controls">
-                <input type="text" name="feedback[email]" />
-              </div>
-            </div>
-
-            <div className="control-group">
-              <label><T id="contact.form.text" />:</label>
-              <div className="controls">
-                <textarea rows="6" cols="30" name="feedback[message]" />
-              </div>
-            </div>
-
-            <button type="submit" className="btn"><T id="contact.form.send" /></button>
-          </form>
+          <Form className={s.form} onSubmit={submit}>
+            {({ pristine, submitting }) => (<>
+              <ContactInput
+                name="name"
+                className="input-block"
+                required value={values.name}
+                onChange={setValue('name')}
+              />
+              <ContactInput
+                name="email"
+                type="email"
+                className="input-block"
+                value={values.email}
+                onChange={setValue('email')}
+              />
+              <ContactInput
+                name="message"
+                type="textarea"
+                required
+                className="input-block"
+                value={values.message}
+                onChange={setValue('message')}
+              />
+              <button type="submit" className={s.btn} disabled={pristine || submitting}>
+                <T id="contact.form.send" />
+              </button>
+            </>)}
+          </Form>
         </div>
 
-        <div>
+        <div className={s.block}>
           <h3><T id="contact.phone" />:</h3>
           <div className={s.phones}>
             {intl.messages.raw.menu.phones
-              .map((phone, index) => <div key={index} dangerouslySetInnerHTML={{ __html: phone }} />)}
+              .map((phone, index) => <p key={index} dangerouslySetInnerHTML={{ __html: phone }} />)}
           </div>
-          <div className="markdown">
+          <div className={s.details}>
             <Thtml id="contact.details" />
           </div>
         </div>
@@ -58,5 +88,3 @@ const Contacts = () => {
     </div>
   )
 }
-
-export default Contacts;
