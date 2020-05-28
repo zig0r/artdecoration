3655ccd4389f3dd2a9023f80c2b0e97f6b945291
Olena-Stotska <i.olena.stotska@gmail.com>
2020-05-28 15:26:15 +0300

    feat(page): adds page not found

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import {
+  FormattedMessage as T,
+  FormattedHTMLMessage as Thtml
+} from 'react-intl';
 
 export default () => (
-  <div className="content">Page not found</div>
+  <div className="content">
+    <h3 className="container-style">
+      <T id="notfound.name" />
+    </h3>
+
+    <div className="markdown">
+      <Thtml id="notfound.content" />
+    </div>
+  </div>
 );
