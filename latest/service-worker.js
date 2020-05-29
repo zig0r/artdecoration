f9e15716c8545753f87eca49c81c8d67cecfcf7f
stalniy <stalniy@users.noreply.github.com>
2020-05-29 14:22:08 +0000

    chore(release): deploy latest

--- latest/service-worker.js
+++ latest/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/latest/precache-manifest.ffaeaa4a65b0259418a18665c6aa29ad.js"
+  "/artdecoration-on-reactjs/latest/precache-manifest.1c41c544b4b319f7d3f673720275a60c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/latest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/artdecoration-on-reactjs/latest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
